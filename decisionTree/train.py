466fa1b419f335210700d3d5b9a2d65e9b37393c
JimmyQYHuang <jimmyqyhuang@ami.com>
2024-10-16 17:06:42 +0800

    add time, record

--- decisionTree/train.py
+++ decisionTree/train.py
@@ -6,9 +6,10 @@ from sklearn.metrics import accuracy_score, classification_report
 from sklearn import tree
 from typing import List
 import matplotlib.pyplot as plt
+import json
 
 
-def Train(features: List, targetLabels: List, feature_names: List[str] = None, class_names: List[str] = None):
+def Train(features: List, targetLabels: List, feature_names: List[str], class_names: List[str], recordPath: str):
   # Step 2: Split the data into training and test sets
   X_train, X_test, y_train, y_test = train_test_split(features, targetLabels, test_size=0.05, random_state=42)
 
@@ -21,14 +22,23 @@ def Train(features: List, targetLabels: List, feature_names: List[str] = None, c
 
   # Step 5: Evaluate the model's performance
   accuracy = accuracy_score(y_test, y_pred)
+  with open(f"{recordPath}/report.json", 'r') as f:
+    data = json.load(f)
+    data["accuracy_score"] = accuracy
+  with open(f"{recordPath}/report.json", 'w') as f:
+    json.dump(data, f, indent=4)
+    
   print(f"Accuracy: {accuracy}")
   print("Classification Report:")
-  print(classification_report(y_test, y_pred))
+  report_dict = classification_report(y_test, y_pred, output_dict=True)
+  # print(classification_report(y_test, y_pred))
+  with open(f"{recordPath}/classification_report.json", 'w') as f:
+    json.dump(report_dict, f, indent=4)
 
   # Step 6: Visualize the decision tree
   plt.figure(figsize=(12,8))
   tree.plot_tree(clf, filled=True, feature_names=feature_names, class_names=class_names)
-  plt.show()
+  plt.savefig(f"{recordPath}/decision_tree.png")
 
 if __name__ == "__main__":
   Train()
--- main.py
+++ main.py
@@ -3,10 +3,19 @@ from decisionTree.train import Train
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from utils.cleanHtmlContent import CleanHtmlContent
+from datetime import datetime
 import numpy as np
 import time
+import os, json
 
 startTime = time.time()
+
+currentTime = datetime.now()
+formattedTime = currentTime.strftime('%Y-%m-%d:%H-%M-%S')
+recordPath = f"records/{formattedTime}"
+os.mkdir(recordPath)
+with open(f"{recordPath}/report.json", 'w') as f:
+    json.dump({}, f, indent=4)
 
 print("get all questions")
 questions = GetAllQuestions()
@@ -20,14 +29,15 @@ for question in questions:
     targetLabels.append(question.Topics)
 mlb = MultiLabelBinarizer()
 targetLabels = mlb.fit_transform(targetLabels)
-class_counts = np.sum(targetLabels, axis=0)
 
-# Get the class names
-class_names = mlb.classes_
+# class_counts = np.sum(targetLabels, axis=0)
 
-# Print class counts for each class
-for class_name, count in zip(class_names, class_counts):
-    print(f"{class_name}: {count}")
+# # Get the class names
+# class_names = mlb.classes_
+
+# # Print class counts for each class
+# for class_name, count in zip(class_names, class_counts):
+#     print(f"{class_name}: {count}")
 
 print("transform features")
 # transform features
@@ -42,7 +52,16 @@ features = vectorizer.fit_transform(features)
 print(len(targetLabels))
 print("training")
 # print(features is None, targetLabels is None, )
-Train(features, targetLabels, vectorizer.get_feature_names_out(), mlb.classes_)
+Train(features, targetLabels, vectorizer.get_feature_names_out(), mlb.classes_, recordPath)
     
-print(f"Execution time: {time.time()-startTime} seconds")
+executionTime = round(time.time()-startTime, 2)
+print(f"Execution time: {executionTime} seconds")
 
+with open(f"{recordPath}/report.json", 'r') as f:
+    data = json.load(f)
+    data["total_questions"] = len(questions)
+    data["total algorithms"] = len(targetLabels)
+    data["execution time"] = executionTime
+with open(f"{recordPath}/report.json", 'w') as f:
+    json.dump(data, f, indent=4)
+    
